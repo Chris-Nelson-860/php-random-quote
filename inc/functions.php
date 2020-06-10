a8a64707e64c83ceb5df15b780ea8ccb794d0ec9
Christopher Nelson <chrismbp@MacBook-Pro.local>
2020-06-10 16:26:22 -0400

    completed the functions to print a random quote.

--- inc/functions.php
+++ inc/functions.php
@@ -1,9 +1,9 @@
-// PHP - Random Quote Generator
+<!-- // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
 // Create the getRandomQuuote function and name it getRandomQuote
-// Create the printQuote funtion and name it printQuote
+// Create the printQuote funtion and name it printQuote -->
 <?php
 $quotes = [
     [
@@ -28,11 +28,24 @@ $quotes = [
     ],
 ];
 
+//this function returns a random index number from the array used in the argument.  It is how i am making sure that i pick a number that is within the range of elements in the array.
+
 function getRandomQuote($arr){
-    $randomNumber = array_rand($arr,1);
-    echo $randomNumber;
+     return array_rand($arr,1);
 }
- 
+//The number from getRandomQuote is stored in a variable 
+$randomNumber = getRandomQuote($quotes);
+
+//The random number gets used as the index number for the random quote.
+function printQuote($arg){
+    global $randomNumber;
+    echo "<p class=\"quote\">" . $arg[$randomNumber]['quote'] . "</p>";
+    echo "<p class=\"source\">" . $arg[$randomNumber]['source'] . "</p>";
+}
+
+printQuote($quotes);
+
+
 
 ?>
 
